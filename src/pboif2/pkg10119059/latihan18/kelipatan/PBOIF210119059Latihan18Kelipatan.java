279845089fd19eaad898dc39e1868c38f6a9a482
irgidp <irgijak@gmail.com>
2020-10-30 14:48:55 +0700

    Tugas PBO

---
+++ src/pboif2/pkg10119059/latihan18/kelipatan/PBOIF210119059Latihan18Kelipatan.java
@@ -0,0 +1,19 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi menampilkan kelipatan 3,5
+ */
+package pboif2.pkg10119059.latihan18.kelipatan;
+
+public class PBOIF210119059Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        float bil;
+        for(bil=1;bil<=70;bil++)
+            if (bil%3.5==0)
+                System.out.println(bil*0.5);
+    }
+    
+}
